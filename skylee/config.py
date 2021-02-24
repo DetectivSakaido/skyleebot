ad30f054087b179c8384e2987f72ae8fc8682460
DetectivSakaido <79519612+DetectivSakaido@users.noreply.github.com>
2021-02-24 11:41:09 +0530

    Update config.py

--- skylee/config.py
+++ skylee/config.py
@@ -4,7 +4,7 @@ from skylee.sample_config import Config
 class Development(Config):
     OWNER_ID =  1387988065 # my telegram ID
     OWNER_USERNAME = "OtakuDxD"  # my telegram username
-    API_KEY = "1469420861:AAEg56tAEcy99QQYFPDOAm53xG4Lmt9iJ7Y"  # my api key, as provided by the botfather
+    API_KEY = "1570223191:AAHJYX8VIbfLJMxlgLKdtaNGqrtVwIdWsV4"  # my api key, as provided by the botfather
     SQLALCHEMY_DATABASE_URI = 'postgres://vjelwrjqcvupwb:1f7e8a7957d4f217bc20f09f7a811b61a4ecabe028e317008894f000fecb4735@ec2-3-231-194-96.compute-1.amazonaws.com:5432/d9jjcovipa4dji'  # sample db credentials
     MESSAGE_DUMP = '-1001191458830' # some group chat that your bot is a member of
     USE_MESSAGE_DUMP = True
