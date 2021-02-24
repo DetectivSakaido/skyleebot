a6cb407b4a73f782faff3dddde94c91a697b6ff7
DetectivSakaido <79519612+DetectivSakaido@users.noreply.github.com>
2021-02-24 11:37:47 +0530

    Update config.py

--- skylee/config.py
+++ skylee/config.py
@@ -2,14 +2,14 @@ from skylee.sample_config import Config
 
 
 class Development(Config):
-    OWNER_ID =  894380120 # my telegram ID
-    OWNER_USERNAME = "starryboi"  # my telegram username
-    API_KEY = "your bot api key"  # my api key, as provided by the botfather
-    SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost:5432/database'  # sample db credentials
-    MESSAGE_DUMP = '-1234567890' # some group chat that your bot is a member of
+    OWNER_ID =  1387988065 # my telegram ID
+    OWNER_USERNAME = "OtakuDxD"  # my telegram username
+    API_KEY = "1469420861:AAEg56tAEcy99QQYFPDOAm53xG4Lmt9iJ7Y"  # my api key, as provided by the botfather
+    SQLALCHEMY_DATABASE_URI = 'postgres://vjelwrjqcvupwb:1f7e8a7957d4f217bc20f09f7a811b61a4ecabe028e317008894f000fecb4735@ec2-3-231-194-96.compute-1.amazonaws.com:5432/d9jjcovipa4dji'  # sample db credentials
+    MESSAGE_DUMP = '-1001191458830' # some group chat that your bot is a member of
     USE_MESSAGE_DUMP = True
     SUDO_USERS = []  # List of id's for users which have sudo access to the bot.
     LOAD = []
     NO_LOAD = []
-    TELETHON_HASH = None # for purge stuffs
-    TELETHON_ID = None
+    TELETHON_HASH = "1c2c4f9d372bdb22958807b17bc33966" # for purge stuffs
+    TELETHON_ID = "1798019"
